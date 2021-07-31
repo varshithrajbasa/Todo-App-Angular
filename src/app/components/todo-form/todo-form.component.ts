4e66c6c83db03d377fb0ebb07cbbc4d743d16769
Basa Varshith Raj <varshithraj.basa@gmail.com>
2021-07-31 13:16:33 +0530

    Created todo form component

---
+++ src/app/components/todo-form/todo-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import  { Todo } from './../../model/todo';
+import { v4 as uuidv4 } from 'uuid';
+import { TodoService } from './../../service/todo.service';
+
+@Component({
+  selector: 'app-todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.scss']
+})
+export class TodoFormComponent implements OnInit {
+todoTitle: string;
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit(): void {
+  }
+  handleAdd(){
+    const newTodo: Todo = {
+      id : uuidv4(),
+      title: this.todoTitle,
+      date: new Date(),
+      isComplete : false
+    }
+    this.todoService.addTodos(newTodo);
+    this.todoTitle = "";
+  }
+
+}
