b678b08af50861e1897cceacd89d0e09eb87ccf1
Basa Varshith Raj <varshithraj.basa@gmail.com>
2021-09-10 20:44:46 +0530

    Added functionality to display todos completed/in complete

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -7,26 +7,58 @@ import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent implements OnInit {
-  faTrashAlt=faTrashAlt;
-  todos:Todo[]
-  constructor(private todoService: TodoService) { }
+  faTrashAlt = faTrashAlt;
+  todos: Todo[];
+  tempTodos: Todo[];
+  activeButtons = {
+    in_complete: false,
+    completed: false,
+    show_all: false,
+  };
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-    this.todoService.getTodos().subscribe(todos =>  {
-      this.todos = todos
-    })
-    
+    this.todoService.getTodos().subscribe((todos) => {
+      this.tempTodos = todos;
+      this.todos = this.tempTodos.filter((ele) => !ele.isComplete);
+      this.setActiveButton(true, false, false);
+    });
   }
 
-  changeTodoStatus(todo: Todo){
+  changeTodoStatus(todo: Todo) {
     this.todoService.changeStatus(todo);
   }
-  deleteTodo(todo: Todo){
-    this.todoService.deleteTodo(todo);  
+  deleteTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo);
+  }
+  showFilteredData(type: number) {
+    if (type === 1) {
+      //incompleted
+      this.todos = this.tempTodos.filter((ele) => !ele.isComplete);
+      this.setActiveButton(true, false, false);
+    } else if (!type) {
+      //completed
+      this.todos = this.tempTodos.filter((ele) => ele.isComplete);
+      this.setActiveButton(false, true, false);
+    } else {
+      // show all
+      this.todos = this.tempTodos;
+      this.setActiveButton(false, false, true);
+    }
   }
 
-
+  setActiveButton(
+    in_complete: boolean = false,
+    completed: boolean = false,
+    show_all: boolean = false
+  ) {
+    this.activeButtons = {
+      in_complete: in_complete,
+      completed: completed,
+      show_all: show_all,
+    };
+  }
 }
