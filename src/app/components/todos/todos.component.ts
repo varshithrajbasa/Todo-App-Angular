2ddde332b46f78f6653c6b1f310bd292e874d2ee
Basa Varshith Raj <varshithraj.basa@gmail.com>
2021-09-11 09:55:40 +0530

    Handling state of application using behaviour subject

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TodoService } from './../../service/todo.service';
 
 import { Todo } from './../../model/todo';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, OnDestroy {
   faTrashAlt = faTrashAlt;
   todos: Todo[];
   tempTodos: Todo[];
@@ -18,22 +20,27 @@ export class TodosComponent implements OnInit {
     completed: false,
     show_all: false,
   };
+  $onDestroySubject: Subject<any> = new Subject();
   constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-    this.todoService.getTodos().subscribe((todos) => {
-      this.tempTodos = todos;
-      this.todos = this.tempTodos.filter((ele) => !ele.isComplete);
-      this.setActiveButton(true, false, false);
+    this.todoService.$todos.pipe(takeUntil(this.$onDestroySubject)).subscribe({
+      next: (todos) => {
+        this.tempTodos = todos;
+        this.todos = this.tempTodos.filter((ele) => !ele.isComplete);
+        this.setActiveButton(true, false, false);
+      },
     });
   }
 
   changeTodoStatus(todo: Todo) {
     this.todoService.changeStatus(todo);
   }
+
   deleteTodo(todo: Todo) {
     this.todoService.deleteTodo(todo);
   }
+
   showFilteredData(type: number) {
     if (type === 1) {
       //incompleted
@@ -60,5 +67,10 @@ export class TodosComponent implements OnInit {
       completed: completed,
       show_all: show_all,
     };
+  }
+
+  ngOnDestroy() {
+    this.$onDestroySubject.next();
+    this.$onDestroySubject.complete();
   }
 }
--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -1,44 +1,52 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
-import { Todo } from "../model/todo";
+import { BehaviorSubject, of } from 'rxjs';
+import { Todo } from '../model/todo';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
   todos: Todo[];
-  constructor() { 
+  $todos: BehaviorSubject<Todo[]> = new BehaviorSubject(null);
+  constructor() {
     this.todos = [
       {
         id: '111',
         isComplete: false,
         date: new Date(),
-        title: 'Hello There, Feel Free to Use Our Todo App'
-      }
-    ]
-    if(!localStorage.getItem('todos'))
-    localStorage.setItem('todos',JSON.stringify(this.todos));
+        title: 'Hello There, Feel Free to Use Our Todo App',
+      },
+    ];
+    if (!localStorage.getItem('todos'))
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    this.todos = JSON.parse(localStorage.getItem('todos'));
+    this.$todos.next(this.todos);
   }
-  getTodos(){
-    this.todos = JSON.parse(localStorage.getItem('todos'))
-    return of(this.todos)
+  getTodos() {
+    this.todos = JSON.parse(localStorage.getItem('todos'));
+    return of(this.todos);
   }
-  addTodos(todo: Todo){
+  addTodos(todo: Todo) {
     this.todos.unshift(todo);
-    localStorage.setItem('todos',JSON.stringify(this.todos));
+    this.$todos.next(this.todos);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
-  changeStatus(todo: Todo){
-    this.todos.map(singleTodo => {
-      if(singleTodo.id == todo.id){
+  changeStatus(todo: Todo) {
+    this.todos.map((singleTodo) => {
+      if (singleTodo.id == todo.id) {
         todo.isComplete = !todo.isComplete;
       }
     });
-    localStorage.setItem('todos',JSON.stringify(this.todos));
+    this.$todos.next(this.todos);
+
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
-  deleteTodo(todo:Todo){
+  deleteTodo(todo: Todo) {
     const indexofTodo = this.todos.findIndex(
       (currentObj) => currentObj.id === todo.id
     );
     this.todos.splice(indexofTodo, 1);
-    localStorage.setItem('todos',JSON.stringify(this.todos));
+    this.$todos.next(this.todos);
+
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
