374cf27c6b9365487901e12febada680e67b5a4d
Varshith Raj Basa <varshithraj.basa@gmail.com>
2026-08-23 23:01:58 +0530

    Update Light theme and few fixes.

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -1,34 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import  { Todo } from './../../model/todo';
+import { Todo } from './../../model/todo';
 import { v4 as uuidv4 } from 'uuid';
 import { TodoService } from './../../service/todo.service';
 
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
-  styleUrls: ['./todo-form.component.scss']
+  styleUrls: ['./todo-form.component.scss'],
 })
 export class TodoFormComponent implements OnInit {
-todoTitle: string;
-isEmpty:Boolean = false;
-  constructor(private todoService:TodoService) { }
+  todoTitle?: string;
+  isEmpty: Boolean = false;
+  constructor(private todoService: TodoService) {}
 
-  ngOnInit(): void {
-  }
-  handleAdd(){
-    if(!this.todoTitle){
+  ngOnInit(): void {}
+  handleAdd() {
+    if (!this.todoTitle) {
       this.isEmpty = true;
       return;
     }
     this.isEmpty = false;
     const newTodo: Todo = {
-      id : uuidv4(),
+      id: uuidv4(),
       title: this.todoTitle,
       date: new Date(),
-      isComplete : false
-    }
+      isComplete: false,
+    };
     this.todoService.addTodos(newTodo);
-    this.todoTitle = "";
+    this.todoTitle = '';
   }
-
 }
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  isDarkMode = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    // Check if the user has a saved preference, otherwise default to light
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    this.isDarkMode = savedTheme === 'dark';
+    document.body.setAttribute('data-theme', savedTheme);
   }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    const currentTheme = this.isDarkMode ? 'dark' : 'light';
+    
+    // 1. Update the DOM attribute
+    document.body.setAttribute('data-theme', currentTheme);
+    
+    // 2. Save preference so it remembers on page refresh
+    localStorage.getItem('theme');
+    localStorage.setItem('theme', currentTheme);
+  }
+  constructor() { }
 
 }
